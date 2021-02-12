15c85dd070692f9f8a11586fe205377d713d81eb
Milo20i <dematteis_danilo@yahoo.it>
2021-02-12 15:52:16 +0100

    adds d10

---
+++ Desktop/d10exam/index.js
@@ -0,0 +1,26 @@
+let x = "John"
+let y = "Doe"
+console.log(x +" "+ y);
+
+let person = {
+    name: "Danilo",
+    surname: "De Matteis",
+    email: "dematteis_danilo@yahoo.it"
+};
+
+delete person.email;
+console.log(person);
+
+
+const animal = ['ant', 'camel', 'duck', 'bison','lion','cat','dog','panther','pinguin','turtle' ];
+console.log(animal);
+
+
+//Array.from({length: 101}, (v, k) => k+1);
+
+const number = []
+
+
+
+
+
