98a9613da461321ef87ab72103f2acb0c724cc21
Milo20i <dematteis_danilo@yahoo.it>
2021-03-09 15:38:31 +0100

    adds w1-d2

---
+++ Desktop/Full Stack Developer March/index.js
@@ -0,0 +1,98 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- The solution must be available for the tutors by the end of the day (5PM CET)
+- You can Google / use StackOverflow BUT we suggest you to use just the material provided
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+
+*/
+/* EXERCISE 1
+Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
+
+
+/* WRITE YOUR ANSWER HERE */
+
+//In JavaScrip there are 6 data types : STRING (The String object is used to represent and manipulate a sequence of characters), NUMBERS, BOOLEAN (is something is True or False), NaN (Not a Number(This method returns true if the value is of the type Number, and equates to NaN. Otherwise it returns false.)), UNDEFINED (when a value is not yet defined) and SYMBOL (which dynamically produces an anonymous, unique value) 
+
+/* EXERCISE 2
+Try to describe what a variable is, in your own words.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+//Variable means anything that can vary. Includes variables which hold the data value and it can be changed anytime. When you declare a variable use "let"
+
+/* EXERCISE 3
+Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+console.log(12+5)
+
+/* EXERCISE 4
+Create a variable named x containing the number 12.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+let x = 12
+console.log(x)
+
+/* EXERCISE 5
+Create a variable called name containing the string John Doe.
+*/
+
+
+/* WRITE YOUR CODE HERE */
+
+let name = "John Doe"
+console.log(name)
+
+/* EXERCISE 6
+Execute a subtraction between the number 12 and the variable x, which is storing the value 12.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+let x = '12'
+console.log(12-x)
+
+/* EXERCISE 7
+Create two variables: name1 and name2. name1 is equal to john, name2 is equal to John.
+Verify that name1 is different from name2. 
+Verify then, that name1 and name2 are equals if both lowercase (without changing the value of name2).
+*/
+
+/* WRITE YOUR CODE HERE */
+
+let name1 = "john"
+let name2 = "John"
+if (name1 === name2.toLowerCase){
+    console.log(john)
+}
+
+
+/* EXERCISE 8
+Create a variable named x (its value must be less than 10). Write the code to print the literal value of x (ex.: if x is 1 print "one", if 5 print "five" etc.).
+*/
+
+/* WRITE YOUR CODE HERE */
+
+let x = 8
+if (x === 8){
+    console.log("eight")
+}
+
+/* EXERCISE 9
+[Extra] Insert a value in a variable based on the resut of a ternary if (topic not covered during lesson, try to search it by yourself, tomorrow morning we'll discuss it together ;) )
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* WHEN YOU ARE FINISHED
+Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
+*/
